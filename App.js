b5280947cbe21fe400f1c72e0f95ea2a35ef77aa
hamzazahid8556205 <129792956+hamzazahid8556205@users.noreply.github.com>
2023-04-04 00:49:20 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import Exercise1 from './Exercise1';
+import Exercise2 from './Exercise2';
+import Exercise3 from './Exercise3';
+import Exercise4 from './Exercise4';
+import AboutUs from './AboutUs';
+import {BrowserRouter, Link, Route, Routes} from 'react-router-dom'
+
+function App() {
+  return (
+    
+    <div className="App">
+      <nav>
+        
+  <Link to='/Exercise1'>Exercise1</Link>
+  <Link to='/Exercise2'>Exercise2</Link>
+  <Link to='/Exercise3'>Exercise3</Link>
+  <Link to='/Exercise4'>Exercise4</Link>
+  <Link to='/about-us'>About Us</Link>
+  
+</nav>
+
+
+
+      <Exercise1 />
+      
+
+
+      <Routes>
+        <Route path = '/Exercise1' element = {<Exercise1/>} />
+        <Route path = '/Exercise2' element = {<Exercise2/>} />
+        <Route path = '/Exercise3' element = {<Exercise3/>} />
+        <Route path = '/Exercise4' element = {<Exercise4/>} />
+        <Route path = '/About-us' element = {<AboutUs/>} />
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
